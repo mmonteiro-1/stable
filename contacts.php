1725d34f8a69b8593ec163be0c5a59622778315e
Matheus Monteiro <mmonteiro.182@gmail.com>
2023-10-16 16:05:02 +0100

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -5,15 +5,15 @@ if(isset($_POST['submit'])){
     $first_name = $_POST['first_name'];
     // $last_name = $_POST['last_name'];
     $subject = "Form submission";
-    // $subject2 = "Copy of your form submission";
+    $subject2 = "Copy of your form submission";
     $message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    // $headers2 = "From:" . $to;
+    $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 ?>
@@ -73,7 +73,7 @@ if(isset($_POST['submit'])){
 					<textarea name="message" id="message" rows="3" placeholder="Enter your message" required></textarea>
 				</div>
 				<div class="field">
-					<button type="submit" name="submit" value="Submit">
+					<button type="submit" name="submit" value="Submit">Send</button>
 				</div>
 			</form>
 			</div>
