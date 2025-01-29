73dd85f99c523a3277e467f95c2cb252ea3e4616
Matheus Monteiro <mmonteiro.182@gmail.com>
2025-01-29 17:31:01 +0000

    major overhaul

--- js/jquery.imageviewer.js
+++ js/jquery.imageviewer.js
@@ -1,73 +1,25 @@
 function imageviewer(selector = 'img') {
-    $(document).on('click', selector, function() {
-        let imageLink = $(this).attr('src');
+	$(document).on('click', selector, function() {
+		const imageLink = $(this).attr('src');
 
-        if (!imageLink) {
-            console.warn('[ImageViewer] Invalid link to image');
-            return;
-        }
+		if ($('#imageviewer-wrapper').length) return;
 
-        // Check if the image viewer already exists
-        if ($('#imageviewer-wrapper').length > 0) {
-            return; // If it exists, do nothing
-        }
-
-        // Prevent scrolling of parent div
-        $('body').css('overflow', 'hidden');
-
-        $('<div>', {
-            id: 'imageviewer-wrapper',
-            css: {
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                zIndex: 99999,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: '100vw',
-                height: '100vh',
-                backgroundColor: '#232323',
-                overflow: 'auto',
-            },
-            append: $('<img>', {
-                id: 'imageviewer-image',
-                css: {
-                    cursor: 'zoom-in',
-                    maxHeight: '90vh', // Adjusted to leave space for close button
-                    maxWidth: '90vw', // Adjusted to leave space for close button
-                },
-                src: imageLink,
-                on: {
-                    click: function(event) {
-                        $(this).toggleClass('expanded');
-                    }
-                }
-            }),
-        }).append(
-            $('<div>', {
-                id: 'imageviewer-close',
-                css: {
-                    position: 'fixed',
-                    top: '20px', // Adjusted for space from top
-                    right: '20px', // Adjusted for space from right
-                    zIndex: 100000,
-                    cursor: 'pointer',
-                    padding: '20px',
-                    backgroundColor: 'white',
-                    backgroundImage: 'url(images/icon_close.png)',
-                    backgroundSize: '20px',
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-                    borderRadius: '50%',
-                },
-                on: {
-                    click: function(event) {
-                        $('#imageviewer-wrapper').remove();
-                        $('body').css('overflow', ''); // Re-enable scrolling of parent div
-                    }
-                }
-            })
-        ).appendTo('body');
-    });
+		$('body').css('overflow', 'hidden').append(`
+			<div id="imageviewer-wrapper">
+				<img id="imageviewer-image" src="${imageLink}">
+				<div id="imageviewer-close"></div>
+				${window.innerWidth < 800 ? '<div id="imageviewer-text">Tap to expand</div>' : ''}
+			</div>
+		`);
+		$('#imageviewer-image').on('click', function() {
+			$(this).toggleClass('expanded');
+			if (window.innerWidth < 800) {
+				$('#imageviewer-text').text($(this).hasClass('expanded') ? 'Tap to contract' : 'Tap to expand');
+			}
+		});
+		$('#imageviewer-close').on('click', function() {
+			$('#imageviewer-wrapper').remove();
+			$('body').css('overflow', '');
+		});
+	});
 }
