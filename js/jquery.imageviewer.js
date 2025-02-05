7305c91c3593aebb8be970008f443c065e7c2f5a
Matheus Monteiro <mmonteiro.182@gmail.com>
2025-02-05 13:54:39 +0000

    remove double zoom

--- js/jquery.imageviewer.js
+++ js/jquery.imageviewer.js
@@ -8,16 +8,9 @@ function imageviewer(selector = '.image > img') {
 			<div id="imageviewer-wrapper">
 				<img id="imageviewer-image" src="${imageLink}">
 				<div id="imageviewer-close"></div>
-				${window.innerWidth < 800 ? '<div id="imageviewer-text">Tap to expand</div>' : ''}
 			</div>
 		`);
-		$('#imageviewer-image').on('click', function() {
-			$(this).toggleClass('expanded');
-			if (window.innerWidth < 800) {
-				$('#imageviewer-text').text($(this).hasClass('expanded') ? 'Tap to contract' : 'Tap to expand');
-			}
-		});
-		$('#imageviewer-close').on('click', function() {
+		$('#imageviewer-image, #imageviewer-close').on('click', function() {
 			$('#imageviewer-wrapper').remove();
 			$('body').css('overflow', '');
 		});
