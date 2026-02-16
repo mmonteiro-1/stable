2b082261d08b493a0dc5e99edcf7ddaab2b545c7
Matheus Monteiro <mmonteiro.182@gmail.com>
2026-02-16 17:25:48 +0000

    tweaks

--- js/jquery.imageviewer.js
+++ js/jquery.imageviewer.js
@@ -18,7 +18,7 @@ function imageviewer(selector = '.image > img, .tile > img') {
 		// Remove existing viewer if present
 		$('#imageviewer-wrapper').remove();
 		
-		$('body').css('overflow', 'hidden').append(`
+		$('body').css('overflow', 'hidden').css('background', '#232323').append(`
 			<div id="imageviewer-wrapper">
 				<img id="imageviewer-image" src="${imageLink}">
 				<div id="imageviewer-close"></div>
@@ -32,7 +32,7 @@ function imageviewer(selector = '.image > img, .tile > img') {
 		// Close viewer
 		$('#imageviewer-image, #imageviewer-close').on('click', function() {
 			$('#imageviewer-wrapper').remove();
-			$('body').css('overflow', '');
+			$('body').css('overflow', '').css('background', '#f2f2f2');
 		});
 
 		// Previous image
